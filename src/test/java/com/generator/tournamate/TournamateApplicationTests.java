54a2b5c5e5ba41deef2d11375409354e85c8e5a5
Armaritto <armia.joseph35@gmail.com>
2023-11-05 23:45:24 +0200

    Initialization

---
+++ src/test/java/com/generator/tournamate/TournamateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generator.tournamate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TournamateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
