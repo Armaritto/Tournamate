e41a61070551e3a751f49b005dd7e91e4fc62f26
Armaritto <armia.joseph35@gmail.com>
2023-11-05 23:45:34 +0200

    Initialization

---
+++ src/frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <TournamentGenerator></TournamentGenerator>
+</template>
+
+<script>
+import TournamentGenerator from './components/TournamentGenerator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TournamentGenerator
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/frontend/vue.config.js
@@ -0,0 +1,14 @@
+// vue.config.js
+module.exports = {
+// https://cli.vueis.org/config/#devserver-proxy
+  devServer: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  }
+}
