8d8789fea5682bbab88480ace9cd0bc149b9e378
Habiba Tarek <147947993+habibatarek26@users.noreply.github.com>
2023-11-11 04:03:42 +0200

    Delete src/RoundRobin/RoundRobin directory

--- src/RoundRobin/RoundRobin/src/main/java/com/RoundRobin/RoundNotFoundException.java
+++
@@ -1,4 +0,0 @@
-package com.RoundRobin;
-
-public class RoundNotFoundException extends Exception{
-}
--- src/RoundRobin/RoundRobin/src/test/java/com/RoundRobin/entities/RoundRobinTeamTest.java
+++
@@ -1,7 +0,0 @@
-package com.RoundRobin.entities;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class RoundRobinTeamTest {
-
-}
